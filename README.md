cf42c2cbcde0cb03c873fbc510161b675a5a9390
Ramoslila817 <Ramoslila817@gmail.com>
2023-05-16 14:22:09 -0700

    create: make entire architecture

---
+++ README.md
@@ -0,0 +1,58 @@
+# E-Commerce Store
+
+A Ecommerce Store made with React.js Framework.
+
+
+## Demo
+
+https://reactjs-ecommerce-app.vercel.app/
+
+## Features
+
+- Easy to integrate with Backend
+- Fully Responsive
+
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/fQ293tm/image.png)
+
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/MildDrizzle/react-ecommerce-store
+```
+
+Go to the project directory
+
+```bash
+  cd react-ecommerce-store
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+## Tech Stack
+
+* [React](https://reactjs.org/)
+* [Redux](https://redux.js.org/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Fake Store API](https://fakestoreapi.com/)
+
+## Contributing
+
+Contributions are always welcome!
+Just raise an issue, we will discuss it.
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        Welcome to our ecommerce store, your ultimate destination for high-quality Clothing, Jewelry, and Electronics. We pride ourselves on offering a carefully curated selection of products that cater to your unique needs and preferences. Our focus is on providing our customers with the latest fashion trends and technological innovations, all at reasonable prices.
+        Our team of experts carefully selects each item in our inventory, ensuring that we always have the latest styles and designs available for you to choose from. We understand how difficult and time-consuming shopping for clothing and accessories can be, which is why we have made our website user-friendly and easy to navigate. Finding the items you want is just a few clicks away, and our secure payment options and fast shipping ensure that you receive your order in a timely manner.
+        At our ecommerce store, we are committed to providing exceptional customer service. Our team is always available to answer your questions, offer advice, and help you with any issues you may face. We're dedicated to making your shopping experience as easy and enjoyable as possible, with a customer support team ready to assist you whenever you need it. Thank you for choosing our ecommerce store as your online shopping destination. We hope you enjoy your shopping experience and look forward to serving you soon.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
